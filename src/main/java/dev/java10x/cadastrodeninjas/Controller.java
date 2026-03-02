6a8fe4be37b2e06123eb105b0c7f864b4f7c8329
Stefany Lins <stefanylins19@gmail.com>
2026-03-02 20:42:07 -0300

    Criado a controller [aula2]

---
+++ src/main/java/dev/java10x/cadastrodeninjas/Controller.java
@@ -0,0 +1,21 @@
+package dev.java10x.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+    return "Essa é minnha primeira mensagem nessa rota";
+
+    }
+
+
+
+
+}
