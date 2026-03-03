3aed5f3ba3258e26d7cc5a986f5bde0231801b2f
Stefany Lins <stefanylins19@gmail.com>
2026-03-02 21:35:20 -0300

    Cria uma entidade no banco de dados a partir do NinjaModel [aula3]

---
+++ src/main/java/dev/java10x/cadastrodeninjas/NinjaModel.java
@@ -0,0 +1,30 @@
+package dev.java10x.cadastrodeninjas;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter //Lombok ele gera os getters e setters automaticamente para os atributos da classe
+@Entity //Entity ele transforma uma classe em uma entidade do banco de dados
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id //Id ele pega o atributo id e transforma ele em uma chave primária do banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //GeneratedValue ele gera um valor automaticamente para o id
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel(){
+
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+}
